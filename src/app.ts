a8119fc41c88c990c91e8a0f2de058f7d3486c8b
anikoszakalymolnar <aniko.szakaly.molnar@gmail.com>
2022-10-20 22:45:47 +0200

    solution_004

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
 
+import { HumanHero } from './hero';
+import { TransformerHero } from './transformer';
+
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +11,9 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: any[] = [{ id: 1, name: 'Joe', sex: 'male', age: 30, health: 100 },
+{ id: 2, name: 'Jake', sex: 'male', age: 74, health: 82 },
+{ id: 3, name: 'Agatha', sex: 'female', age: 22, health: 98 }];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,6 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: any[] = [{ id: 1, name: 'Peter', wings: 2, wheels: 1, clan: 'elf' },
+{ id: 2, name: 'Panna', wings: 4, wheels: 2, clan: 'dwarf' },
+{ id: 3, name: 'Lea', wings: 6, wheels: 4, clan: 'fairy' }];
